3425e82a34e6f4a1e778e8fbc18b8c8a63171a6a
Solonos <solon.grover@gmail.com>
2023-09-16 11:49:05 -0600

    Primitive Pie Chart
    Got the foundation for a pie chart going

---
+++ js/PieChart.js
@@ -0,0 +1,69 @@
+/** Class representing a PieChart. */
+class PieChart {
+  /**
+   * @param {json[]} json - array of json objects with outcome and frequency fields
+   */
+  constructor(json) {
+    this.categories = json;
+  }
+
+  /**
+   * Function that renders the piechart
+   */
+  renderPieChart() {
+    // Create an SVG element for the chart
+    var width = 400;
+    var height = 400;
+    var radius = Math.min(width, height) / 2;
+    
+    var svg = d3.select("#pie-chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Generate the pie chart layout
+    var pie = d3.pie()
+        .value(function(d) { return parseFloat(d.frequency); });
+    
+    // Append SVG path elements for each pie slice
+    var arc = svg.selectAll(".arc")
+        .data(pie(this.categories))
+        .enter()
+        .append("g")
+        .attr("class", "arc");
+
+    // Colors for the chart
+    var colorScale = d3.scaleOrdinal().range(["#ffffff","#000000","#a1a1a1"]);
+
+    // Path stroke
+    arc.append("path")
+        .attr("d", d3.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(0))
+        .attr("fill", function(d, i) { return colorScale(i); })
+        .attr("stroke-width", "1px")
+        .attr("stroke", "magenta") // Add a stroke for better separation
+
+    // Add labels with improved positioning
+    arc.append("text")
+        .attr("transform", function(d) {
+            var centroid = d3.arc()
+            .outerRadius(radius - 40) // Adjust outer radius for label positioning
+            .innerRadius(0)
+            .centroid(d);
+            return "translate(" + centroid + ")";
+        })
+        .attr("text-anchor", "middle")
+        .attr("fill", "White")
+        .attr("font-family","Helvetica")
+        .attr("font-weight", 900)
+        .attr("stroke-width", "1px")
+        .attr("stroke", "red")
+        .attr("font-size", ".8em")
+        .text(function(d) {
+            return `${d.data.outcome}: ${(d.data.frequency * 100).toFixed(2)}%`;
+        });
+  }    
+}
---
+++ js/script.js
@@ -0,0 +1,26 @@
+/**
+* Requests the file and executes a callback with the parsed result once
+* it is available
+* @param {string} path - The path to the file.
+* @param {function} callback - The callback function to execute once the result is available
+*/
+function fetchJSONFile (path, callback) {
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        const data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      }
+    }
+  };
+  httpRequest.open('GET', path);
+  httpRequest.send();
+}
+
+// call fetchJSONFile then build and render a tree
+// this is the function executed as a callback when parsing is done
+fetchJSONFile('data/GameOutcomes.json', function (data) {
+  const tree = new PieChart(data);
+  tree.renderPieChart();
+});

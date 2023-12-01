9366a884ed06022c67876ebfb821e192ea8d4b7d
Solonos <solon.grover@gmail.com>
2023-11-30 18:43:21 -0700

    Histogram prettier now

--- js/HistogramChart.js
+++ js/HistogramChart.js
@@ -1,24 +1,24 @@
 /** Class representing a Histogram. */
 class HistogramChart {
     /**
-     * @param {Array} json - array of JSON objects with length field
+     * @param {Array} json - array of JSON objects with value and freq fields
      */
     constructor(json) {
         this.data = json.flatMap(({ value, freq }) =>
-            Array.from({ "length": freq }, () => value)
+            Array.from({ length: freq }, () => value)
         );
     }
 
     /**
-    * Function that renders the histogram.
-    */
+     * Function that renders the histogram.
+     */
     renderHistogramChart() {
         // Constants for the chart that would be useful.
-        const CHART_WIDTH = 500;
-        const CHART_HEIGHT = 250;
+        const CHART_WIDTH = 900;
+        const CHART_HEIGHT = 500;
         const MARGIN = { left: 70, bottom: 20, top: 20, right: 20 };
         const ANIMATION_DURATION = 300;
-        console.log(this.data)
+
         let histogramChartSvg;
         histogramChartSvg = d3
             .select("#HistogramChart-div")
@@ -30,10 +30,11 @@ class HistogramChart {
             .attr("id", "HistogramChart-svg")
             .attr("class", "histogram");
 
-        // Parse the 'length' field of each data point as integers
-        const parsedData = this.data.map(d => parseInt(d.length));
-
-        const histogramData = d3.histogram()(this.data)
+        const binWidth = 5;
+        const histogramData = d3.histogram()
+            .domain(d3.extent(this.data))
+            .thresholds(d3.range(d3.min(this.data), d3.max(this.data) + binWidth, binWidth))
+            (this.data);
 
         // X axis
         const x = d3
@@ -44,7 +45,7 @@ class HistogramChart {
         // Y axis
         const y = d3
             .scaleLinear()
-            .domain([0, d3.max(histogramData, d=> d.length)]) // Set the domain to the maximum frequency value
+            .domain([0, d3.max(histogramData, d => d.length)])
             .nice()
             .range([CHART_HEIGHT - MARGIN.top - MARGIN.bottom, 0]);
 
@@ -55,20 +56,41 @@ class HistogramChart {
             .enter()
             .append("rect")
             .attr("class", "bar")
-            .attr("x", d => x(d.x0) + 1)
+            .attr("x", d => x(d.x0))
             .attr("y", d => y(d.length))
             .attr("width", d => Math.max(0, x(d.x1) - x(d.x0) - 1))
-            .attr("height", d => CHART_HEIGHT - MARGIN.top - MARGIN.bottom - y(d.length));
-
-        bars
-            .transition()
-            .duration(ANIMATION_DURATION)
-            .attr("x", d => x(d.x0) + 1)
-            .attr("y", d => y(d.length))
-            .attr("width", d => Math.max(0, x(d.x1) - x(d.x0) - 1))
-            .attr("height", d => y(0) - y(d.length));
+            .attr("height", d => CHART_HEIGHT - MARGIN.top - MARGIN.bottom - y(d.length))
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
 
         bars.exit().remove();
+
+        let tooltip = d3
+            .select("#HistogramChart-div")
+            .append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+        // Add hover effect and tooltip using a separate function
+        function handleMouseOver(d) {
+            const bar = d3.select(this);
+            bar.attr("fill", "lightgray"); // Change color on hover
+        
+            const range = `${d.srcElement.__data__.x0} - ${d.srcElement.__data__.x1}`;
+            const length = d.srcElement.__data__.length;
+        
+            tooltip.transition().duration(200).style("opacity", 0.9);
+            tooltip.html(`Range: ${range} moves<br>Games: ${length.toLocaleString("en-US")}`)
+                .style("left", (d.pageX) + "px")
+                .style("top", (CHART_HEIGHT + MARGIN.top) + "px"); // Adjust the top position
+        
+        }
+
+        function handleMouseOut() {
+            const bar = d3.select(this);
+            bar.attr("fill", "5cd34f"); // Revert color on mouseout
+            tooltip.transition().duration(500).style("opacity", 0);
+        }
 
         // X axis
         histogramChartSvg
@@ -102,7 +124,4 @@ class HistogramChart {
             .style("text-anchor", "middle")
             .text("Number of Games");
     }
-
-
-
 }

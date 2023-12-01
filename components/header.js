a0791a72283c048782fd9ab8ead93e9fa25dad07
benruckman <ben.ruckman@live.com>
2023-11-30 19:38:43 -0700

    Added "demo video" to dashboard

--- components/header.js
+++ components/header.js
@@ -7,6 +7,7 @@ class Header extends HTMLElement {
     <nav class="navbar">
         <div>
         <ul>
+        <li><a class="nav-link" href="/">Demo Video</a></li>
         <li><a class="nav-link" href="chess-board.html">Chess Board</a></li>
         <li><a class="nav-link" href="pie-chart.html">Pie Chart</a></li>
         <li><a class="nav-link" href="histogram-chart.html">Histogram</a></li>
